32a32812a40fdd149c8c11f03b29cc8b0478bbc1
Umasankar <39300990+umasankar-ts@users.noreply.github.com>
2020-05-21 13:29:47 +0530

    Update SidebarComponent.js

--- src/component/SidebarComponent.js
+++ src/component/SidebarComponent.js
@@ -4,6 +4,13 @@ class SidebarComponent extends Component {
     render() {
         return (
             <div className="sidebar-content">
+                <div className="primary-dark-bg profile-pic-section" align="center">
+                    <div className="profile-pic-border">
+                    <img src="/images/profile-pic.png" className="img-fluid profile-image"/>
+                    </div>
+                    <h5 className="white-font margin-t-20">T.S. UMASANKAR</h5>
+                    <p className="white-font">umasankar.tsj@gmail.com</p>
+                </div>
                 <ul className="sidebar-nav-menu">
                     <li><a href="#"> <i className="lni-home sidebar-nav-menu-icon secondary-dark-font"></i>Home</a></li>
                     <li><a href="#"> <i className="lni-list sidebar-nav-menu-icon secondary-dark-font"></i>About</a></li>
