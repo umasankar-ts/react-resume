b8ef9cff8eec29c72158a512bab2ecaa030a74b6
Narmatha <umasankar.tsj@gmail.com>
2020-05-23 15:15:16 +0530

    Highlights added to Home component

--- src/component/HomeComponent.js
+++ src/component/HomeComponent.js
@@ -25,69 +25,143 @@ class HomeComponent extends Component {
 	            </div>
 	            <div className="row">
 	            	<div className="col-xl-8 col-lg-12 col-md-12 home-section-space">
-	            		<div className="card-style white-bg">
-	            			adasdas
+	            		<h6 className="fancy-title primary-light-font">Highlights</h6>
+	            		<div className="highlight-view highlight-card-top-border">
+	            			<div className="highlight-content highlight-bottomview highlight-view-left highlight-view-right">
+	            				<div className="row">
+	            					<div className="col-md-1" align="center">
+										<div class="highlight-icon green-gradient">
+                                			<i class="lni-cog"></i>
+                            			</div>
+	            					</div>
+	            					<div className="col-md-11">
+	            						<p>Known for writing efficient, maintainable and reusable code</p>
+	            					</div>
+	            				</div>
+	            			</div>
+	        			</div>
+	            		<div className="dashed-border"></div>
+	            		<div className="highlight-view highlight-topview highlight-view-left highlight-view-right">
+	            			<div className="highlight-content highlight-bottomview highlight-view-left highlight-view-right">
+	            				<div className="row">
+	            					<div className="col-md-1" align="center">
+		            					<div class="highlight-icon orange-gradient">
+	                                		<i class="lni-layout"></i>
+	                            		</div>
+	            					</div>
+	            					<div className="col-md-11">
+	            						<p>Proficient in design, data structures, problem-solving, and debugging</p>
+	            					</div>
+	            				</div>
+	            			</div>
+	        			</div>
+	        			<div className="dashed-border"></div>
+	        			<div className="highlight-view highlight-topview highlight-view-left highlight-view-right">
+	            			<div className="highlight-content highlight-bottomview highlight-view-left highlight-view-right">
+	            				<div className="row">
+	            					<div className="col-md-1" align="center">
+		            					<div class="highlight-icon pale-green-gradient">
+			                                <i class="lni-grid"></i>
+			                            </div>
+	            					</div>
+	            					<div className="col-md-11">
+	            						<p>Expert in the interaction between various devices and versions of Android</p>
+	            					</div>
+	            				</div>			            			
+	            			</div>
+	        			</div>
+	        			<div className="dashed-border"></div>
+	        			<div className="highlight-view highlight-topview highlight-view-left highlight-view-right">
+	            			<div className="highlight-content highlight-bottomview highlight-view-left highlight-view-right">
+	            				<div className="row">
+	            					<div className="col-md-1" align="center">
+            							<div class="highlight-icon pink-gradient">
+                                			<i class="lni-mobile"></i>
+                            			</div>
+	            					</div>
+	            					<div className="col-md-11">
+	            						<p>Ability to design for scale and Performance</p>
+	            					</div>
+    							</div>
+	            			</div>
+	        			</div>
+	        			<div className="dashed-border"></div>
+	        			<div className="highlight-view highlight-topview highlight-view-left highlight-view-right highlight-card-bottom-border">
+	            			<div className="highlight-content">
+	            				<div className="row">
+	            					<div className="col-md-1" align="center">
+            							<div class="highlight-icon blue-gradient">
+                                			<i class="lni-android"></i>
+                        				</div>
+	            					</div>
+	            					<div className="col-md-11">
+	            						<p>Created 3+ fully functional applications for Android devices</p>
+	            					</div>
+    							</div>
+	            			</div>
 	        			</div>
 	            	</div>
 	            	<div className="col-xl-4 col-lg-8 col-md-9 home-section-space">
 	            		<div className="card-style primary-light-bg">
 			            	<h6 className="fancy-title white-font margin-bt-none">Personal Info</h6>
 			            	<hr className="secondary-dark-bg personal-info-hr"/>
-			            	<div className="row">
-			            		<div className="col-md-6">
-			            			<span>Name</span>
-			            		</div>
-			            		<div className="col-md-6">
-			            			<p className="white-font">T.S. Umasankar</p>
-		            			</div>
-			            	</div>
-			            	<div className="row">
-			            		<div className="col-md-6">
-			            			<span>Father Name</span>
-			            		</div>
-			            		<div className="col-md-6">
-			            			<p className="white-font">T.R. Surendran</p>
-		            			</div>
-			            	</div>
-			            	<div className="row">
-			            		<div className="col-md-6">
-			            			<span>Date of Birth</span>
-			            		</div>
-			            		<div className="col-md-6">
-			            			<p className="white-font">04/June/1992</p>
-		            			</div>
-			            	</div>
-			            	<div className="row">
-			            		<div className="col-md-6">
-			            			<span>Marital Status</span>
-			            		</div>
-			            		<div className="col-md-6">
-			            			<p className="white-font">Married</p>
-		            			</div>
-			            	</div>
-			            	<div className="row">
-			            		<div className="col-md-6">
-			            			<span>Gender</span>
-			            		</div>
-			            		<div className="col-md-6">
-			            			<p className="white-font">Male</p>
-		            			</div>
-			            	</div>
-			            	<div className="row">
-			            		<div className="col-md-6">
-			            			<span>Mobile Number</span>
-			            		</div>
-			            		<div className="col-md-6">
-			            			<p className="white-font">8807527642</p>
-		            			</div>
-			            	</div>
-			            	<div className="row">
-			            		<div className="col-md-6">
-			            			<span>Address</span>
-			            		</div>
-			            		<div className="col-md-6">
-			            			<p className="white-font">99, Valluvar Colony, P&T Nagar road, Madurai - 625014</p>
-		            			</div>
+			            	<div className="personal-info-sec">
+				            	<div className="row">
+				            		<div className="col-md-6">
+				            			<span>Name</span>
+				            		</div>
+				            		<div className="col-md-6">
+				            			<p className="white-font">T.S. Umasankar</p>
+			            			</div>
+				            	</div>
+				            	<div className="row">
+				            		<div className="col-md-6">
+				            			<span>Father Name</span>
+				            		</div>
+				            		<div className="col-md-6">
+				            			<p className="white-font">T.R. Surendran</p>
+			            			</div>
+				            	</div>
+				            	<div className="row">
+				            		<div className="col-md-6">
+				            			<span>Date of Birth</span>
+				            		</div>
+				            		<div className="col-md-6">
+				            			<p className="white-font">04/June/1992</p>
+			            			</div>
+				            	</div>
+				            	<div className="row">
+				            		<div className="col-md-6">
+				            			<span>Marital Status</span>
+				            		</div>
+				            		<div className="col-md-6">
+				            			<p className="white-font">Married</p>
+			            			</div>
+				            	</div>
+				            	<div className="row">
+				            		<div className="col-md-6">
+				            			<span>Gender</span>
+				            		</div>
+				            		<div className="col-md-6">
+				            			<p className="white-font">Male</p>
+			            			</div>
+				            	</div>
+				            	<div className="row">
+				            		<div className="col-md-6">
+				            			<span>Mobile Number</span>
+				            		</div>
+				            		<div className="col-md-6">
+				            			<p className="white-font">8807527642</p>
+			            			</div>
+				            	</div>
+				            	<div className="row">
+				            		<div className="col-md-6">
+				            			<span>Address</span>
+				            		</div>
+				            		<div className="col-md-6">
+				            			<p className="white-font">99, Valluvar Colony, P&T Nagar road, Madurai - 625014</p>
+			            			</div>
+				            	</div>
 			            	</div>
 	            		</div>
 	            	</div>
