d035d45ce1606b21aee56343465c1f9476c0eab5
Narmatha <umasankar.tsj@gmail.com>
2020-05-30 19:52:15 +0530

    Education component content added

--- src/component/EducationComponent.js
+++ src/component/EducationComponent.js
@@ -11,6 +11,37 @@ class EducationComponent extends Component{
                     </div>
                     <div className="col-xl-10 col-lg-8 col-md-8 col-sm-7 content-wrapper">
                 	 	<h6 className="fancy-title primary-light-font">Education</h6>
+                        <div className="row exp-sec-space">
+                            <div className="col-xl-8 col-md-12 edu-sec-space">
+                                <div className="edu-card white-bg">
+                                    <div className="row">
+                                        <div className="col-md-10">
+                                            <h6><b>M.Sc</b> (Computer Science & Information Technology)</h6>
+                                            <p className="secondary-dark-font">Sourashtra College, Madurai</p>
+                                            <p>July 2014 - April 2016</p>
+                                        </div>
+                                        <div className="col-md-2 vertical-center">
+                                        <h5>82%</h5>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="edu-card white-bg">
+                                    <div className="row">
+                                        <div className="col-md-10">
+                                            <h6><b>B.Sc</b> (Computer Science)</h6>
+                                            <p className="secondary-dark-font">Sourashtra College, Madurai</p>
+                                            <p>July 2014 - April 2016</p>
+                                        </div>
+                                        <div className="col-md-2 vertical-center">
+                                        <h5>72%</h5>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col-xl-4 col-md-12 vertical-center" align="center">
+                                <img src="/images/graduate.png" className="img-fluid"/>
+                            </div>
+                        </div>
             	 	</div>
         	 	</div>
     	 	</div>
