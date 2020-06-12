9d6a09d8e9c84e2c486953c9800a4e6e497574dd
Narmatha <umasankar.tsj@gmail.com>
2020-06-12 22:29:23 +0530

    Slick slider dependency installed and image slider added for UI Component

--- src/component/UiComponent.js
+++ src/component/UiComponent.js
@@ -1,9 +1,35 @@
 import React, {Component} from 'react';
 import SidebarComponent from './SidebarComponent';
 import MessageComponent from './MessageComponent';
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 class UiComponent extends Component{
 	render(){
+        var settings = {
+            dots: false,
+            infinite: true,
+            speed: 700,
+            slidesToShow: 4,
+            slidesToScroll: 1,
+             responsive: [
+                {
+                breakpoint: 767,
+                    settings: {
+                        slidesToShow: 1,
+                        slidesToScroll: 1                        
+                    }
+                },
+                {
+                    breakpoint: 1024,
+                    settings: {
+                        slidesToShow: 2,
+                        slidesToScroll: 1                        
+                    }
+                }
+            ]
+        };
 		return(
 		 	<div>
                 <div className="sidebar primary-light-bg scrollbar">
@@ -17,13 +43,22 @@ class UiComponent extends Component{
                         <h6 className="fancy-title primary-light-font">Project UIs</h6>
                     	<div className="row">
                             <div className="col-md-12">
-                            
+                                <Slider {...settings}>
+                                    <img src="/images/ui1.png" className="img-fluid"/>
+                                    <img src="/images/ui2.png" className="img-fluid"/>
+                                    <img src="/images/ui3.png" className="img-fluid"/>
+                                    <img src="/images/ui4.png" className="img-fluid"/>
+                                    <img src="/images/ui5.png" className="img-fluid"/>
+                                    <img src="/images/ui6.png" className="img-fluid"/>
+                                    <img src="/images/ui7.png" className="img-fluid"/>
+                                    <img src="/images/ui8.png" className="img-fluid"/>
+                                    <img src="/images/ui9.png" className="img-fluid"/>
+                                </Slider>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-
 		)
 	}
 }
