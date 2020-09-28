05b0c886a0f0bfea7b9cc84557e91079536f033a
Narmatha <umasankar.tsj@gmail.com>
2020-09-28 21:10:38 +0530

    Screen orientation background color changed

--- src/component/MessageComponent.js
+++ src/component/MessageComponent.js
@@ -4,8 +4,8 @@ class MessageComponent extends Component{
 	render(){
 		return(
 		 	<div className="message" align="center">
-                <h5 className="secondary-dark-font">Please rotate your phone</h5>
-                <p className="secondary-dark-font">We don't support portrait mode. Please go back to landscape mode for the best experience.</p>
+                <h5 className="secondary-light-font">Please rotate your phone</h5>
+                <p className="secondary-light-font">We don't support portrait mode. Please go back to landscape mode for the best experience.</p>
             </div>
 		)
 	}
