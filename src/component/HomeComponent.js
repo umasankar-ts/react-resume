92ff22d7ff53e1caa4b1670e3dc30c711c6a0dd7
Narmatha <umasankar.tsj@gmail.com>
2020-09-30 20:34:16 +0530

    Lock orientation script added

--- src/component/HomeComponent.js
+++ src/component/HomeComponent.js
@@ -16,12 +16,12 @@ class HomenewComponent extends Component {
 	    	 		<div className="container">
 	    	 			<div className="animate-gif">
 	    	 				<img src="images/anim-img1.png" className="img-fluid home-gif1 move-up-down" alt="Animation object1"/>
-	    	 				<img src="images/anim-img1.png" className="img-fluid home-gif2 swing-anim" alt="Animation object2"/>
+	    	 				<img src="images/anim-img1.png" className="img-fluid home-gif2 spin-anim" alt="Animation object2"/>
 	    	 				<img src="images/anim-img2.png" className="img-fluid home-gif3 running-anim" alt="Animation object3"/>
 	    	 				<img src="images/anim-img2.png" className="img-fluid home-gif4 flip-anim" alt="Animation object4"/>
 	    	 				<img src="images/anim-img4.png" className="img-fluid home-gif5 rotate-anim" alt="Animation object5"/>
 	    	 				<img src="images/anim-img3.png" className="img-fluid home-gif6 rotate-anim" alt="Animation object6"/>
-	    	 				<img src="images/anim-img2.png" className="img-fluid home-gif7 rotate-anim" alt="Animation object7"/>
+	    	 				<img src="images/anim-img2.png" className="img-fluid home-gif7 spin-anim" alt="Animation object7"/>
 	    	 				<img src="images/anim-img1.png" className="img-fluid home-gif8 run-anim" alt="Animation object8"/>
 	    	 			</div>
     	 			 	<div className="row home-sec align-items-center">
--- src/component/InitialComponent.js
+++ src/component/InitialComponent.js
@@ -2,17 +2,27 @@ import React, {Component} from 'react';
 import {NavLink} from 'react-router-dom';
 
 class InitialComponent extends	Component{
+	lock(){
+		window.screen.orientation.lock('landscape').catch(function(error) {
+    		console.log('dsfdsfjodsi')
+		});
+	}
 	render(){
 		return(
 			<div className="initial-bg">
 	            <div className="fingerprint-section text-center w-100">
 	            	<NavLink to="/home" id="thumb-impression"> 
-	                	<img src="../images/fingerprint.png" className="img-fluid finger-print-img" alt="Fingerprint icon"/> 
+	                	<img src="../images/fingerprint.png" className="img-fluid finger-print-img" onClick={this.lock} alt="Fingerprint icon"/> 
 	            	</NavLink>                               
 	            </div>
-            </div>
+            </div> 
 		)
 	}
 
 }
 export default InitialComponent
+
+/*console.log('sdjfdsj');
+		window.addEventListener("orientationchange", function(event) {
+  			console.log("the orientation of the device is now " + event.target.screen.orientation.angle);
+		});*/
