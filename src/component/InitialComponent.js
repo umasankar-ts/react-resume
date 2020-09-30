6e7da664ea4d87087a7a4632f9f4efbefa750fea
Narmatha <umasankar.tsj@gmail.com>
2020-09-30 20:58:01 +0530

    Lock orientation script added in index

--- src/component/InitialComponent.js
+++ src/component/InitialComponent.js
@@ -1,18 +1,13 @@
 import React, {Component} from 'react';
 import {NavLink} from 'react-router-dom';
 
-class InitialComponent extends	Component{
-	lock(){
-		window.screen.orientation.lock('landscape').catch(function(error) {
-    		console.log('dsfdsfjodsi')
-		});
-	}
+class InitialComponent extends	Component{	
 	render(){
 		return(
 			<div className="initial-bg">
 	            <div className="fingerprint-section text-center w-100">
 	            	<NavLink to="/home" id="thumb-impression"> 
-	                	<img src="../images/fingerprint.png" className="img-fluid finger-print-img" onClick={this.lock} alt="Fingerprint icon"/> 
+	                	<img src="../images/fingerprint.png" className="img-fluid finger-print-img" alt="Fingerprint icon"/> 
 	            	</NavLink>                               
 	            </div>
             </div> 
@@ -21,8 +16,3 @@ class InitialComponent extends	Component{
 
 }
 export default InitialComponent
-
-/*console.log('sdjfdsj');
-		window.addEventListener("orientationchange", function(event) {
-  			console.log("the orientation of the device is now " + event.target.screen.orientation.angle);
-		});*/
