d4d3c350bc56179a1dcfdd94e973287e83a89b37
Narmatha <umasankar.tsj@gmail.com>
2020-09-30 21:13:42 +0530

    Lock orientation function modified

--- src/component/InitialComponent.js
+++ src/component/InitialComponent.js
@@ -6,8 +6,8 @@ class InitialComponent extends	Component{
 		return(
 			<div className="initial-bg">
 	            <div className="fingerprint-section text-center w-100">
-	            	<NavLink to="/home" id="thumb-impression"> 
-	                	<img src="../images/fingerprint.png" className="img-fluid finger-print-img" alt="Fingerprint icon"/> 
+	            	<NavLink to="/home"> 
+	                	<img src="../images/fingerprint.png" id="thumb-impression" className="img-fluid finger-print-img" alt="Fingerprint icon"/> 
 	            	</NavLink>                               
 	            </div>
             </div> 
