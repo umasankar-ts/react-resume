4f56dfda81d42779300e7387913a1f74f724523f
Narmatha <umasankar.tsj@gmail.com>
2020-09-30 21:29:27 +0530

    Screen orientation code modification

--- src/component/InitialComponent.js
+++ src/component/InitialComponent.js
@@ -1,13 +1,18 @@
 import React, {Component} from 'react';
 import {NavLink} from 'react-router-dom';
 
-class InitialComponent extends	Component{	
+class InitialComponent extends	Component{
+	lock(){
+		window.screen.orientation.lock('landscape').catch(function(error) {
+    		//console.log('')
+		});
+	}
 	render(){
 		return(
 			<div className="initial-bg">
 	            <div className="fingerprint-section text-center w-100">
 	            	<NavLink to="/home"> 
-	                	<img src="../images/fingerprint.png" id="thumb-impression" className="img-fluid finger-print-img" alt="Fingerprint icon"/> 
+	                	<img src="../images/fingerprint.png" className="img-fluid finger-print-img" onClick={this.lock} alt="Fingerprint icon"/> 
 	            	</NavLink>                               
 	            </div>
             </div> 
