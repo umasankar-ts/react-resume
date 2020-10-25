6f5c2e4cbe93a99ab21c74c9da1725b705dbfdbb
Narmatha <umasankar.tsj@gmail.com>
2020-10-25 17:37:53 +0530

    server.js file updated

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
+'use strict'
+require('dotenv').config()
+const consts = require('./consts/consts')
+const httpsRedirect = require('express-https-redirect')
+
+console.log(consts.database)
 const express = require('express');
 const favicon = require('express-favicon');
 const path = require('path');
 const port = process.env.PORT || 8080;
 const app = express();
+app.use('/', httpsRedirect())
+app.use(express.static('dist/public'))
+
 app.use(favicon(__dirname + '/build/favicon.ico'));
 // the __dirname is the current directory from where the script is running
 app.use(express.static(__dirname));
@@ -15,16 +24,3 @@ app.get('/*', function (req, res) {
 });
 app.listen(port);
 
-// set up plain http server
-var http = express();
-
-// set up a route to redirect http to https
-http.get('*', function(req, res) {  
-    res.redirect('https://' + req.headers.host + req.url);
-
-    // Or, if you don't want to automatically detect the domain name from the request header, you can hard code it:
-    // res.redirect('https://example.com' + req.url);
-})
-
-// have it listen on 8080
-http.listen(8080);
