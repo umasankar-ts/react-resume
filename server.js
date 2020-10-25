d8a4e79085254443f2f5de1180c6b39ea887175a
Narmatha <umasankar.tsj@gmail.com>
2020-10-25 17:42:26 +0530

    server.js file updated

--- server.js
+++ server.js
@@ -1,17 +1,8 @@
-'use strict'
-require('dotenv').config()
-const consts = require('./consts/consts')
-const httpsRedirect = require('express-https-redirect')
-
-console.log(consts.database)
 const express = require('express');
 const favicon = require('express-favicon');
 const path = require('path');
 const port = process.env.PORT || 8080;
 const app = express();
-app.use('/', httpsRedirect())
-app.use(express.static('dist/public'))
-
 app.use(favicon(__dirname + '/build/favicon.ico'));
 // the __dirname is the current directory from where the script is running
 app.use(express.static(__dirname));
@@ -23,4 +14,3 @@ app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 app.listen(port);
-
